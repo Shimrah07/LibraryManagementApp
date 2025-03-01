f0ef4b50fcb002a22718a7874d740e04c3370623
Shimrah07 <shimrahahmedsa27@gmail.com>
2025-03-01 17:24:13 +0530

    styled the home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchBooks } from "@/redux/actions/booksActions";
+import { Button, Grid, GridItem } from "@chakra-ui/react";
+
 
 const Home = () => {
     const dispatch = useDispatch();
@@ -13,18 +15,20 @@ const Home = () => {
     if (loading) return <h2>Loading...</h2>;
     if (error) return <h2>Error: {error}</h2>;
 
+   
     return (
         <div>
             <h1>Available Books</h1>
-            <div className="books-container">
+            <Grid templateColumns="repeat(4, 1fr)" gap={6} className="books-container">
                 {books.map((book) => (
-                    <div key={book.id} className="book-card">
+                    <GridItem key={book.id} p={4} boxShadow="md" borderRadius="md" textAlign={"center"} gap={4} >
                         <img src={book.coverImage} alt={book.title} />
                         <h3>{book.title}</h3>
                         <p>Author: {book.author}</p>
-                    </div>
+                        <Button>Want to Read</Button>
+                    </GridItem>
                 ))}
-            </div>
+            </Grid>
         </div>
     );
 };
